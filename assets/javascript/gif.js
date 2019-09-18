f17d2e8113f8c30a6499795923771876e36df2d8
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-17 23:43:14 -0500

    buttons populating with user search. need to clear input box

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,7 +3,7 @@ var animals = ["llama", "monkey", "penguin"];
 
 //function for displaying animal data
 function renderButtons(){
-    $("buttons-view").empty();
+    $("#buttons-view").empty();
     for (var i=0; i < animals.length; i++) {
         var a = $("<button>");
         a.addClass("animal");
@@ -19,7 +19,7 @@ $("#add-animal").on("click", function(event){
     var animal = $("#animal-input").val().trim();
     animals.push(animal);
     renderButtons();
-    console.log(animal);
-})
+    // console.log(animal);
+});
 
 renderButtons();
