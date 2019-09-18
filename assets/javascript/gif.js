dceef17fb6cc5a7e08674e85d593612265ed1486
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-17 23:22:44 -0500

    initial array of animals are showing on buttons

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,5 +3,14 @@ var animals = ["llama", "monkey", "penguin"];
 
 //function for displaying animal data
 function renderButtons(){
-    
-}
+    $("buttons-view").empty();
+    for (var i=0; i < animals.length; i++) {
+        var a = $("<button>");
+        a.addClass("animal");
+        a.attr("data-name", animals[i]);
+        a.text(animals[i]);
+        $("#buttons-view").append(a);
+    }
+};
+
+renderButtons();
